fe0f22f7e6657d8d8618bbdacf21f5af857be30b
hongbotian <tianhongbo1@gmail.com>
2016-05-14 16:29:51 -0700

    first release with basic function

---
+++ src/main/java/com/huami/id/zuulserver/ZuulServerApplication.java
@@ -0,0 +1,19 @@
+package com.huami.id.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Scott Tian
+ */
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZuulServerApplication.class, args);
+  }
+
+}
